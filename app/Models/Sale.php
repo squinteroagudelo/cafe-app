a2cdd61a28ebc1dddb6dc285474d81dda667c9b1
Samuel Ricardo Quintero Agudelo <squinteroagudelo18@gmail.com>
2022-06-30 20:58:16 -0500

    creating migrations / models

---
+++ app/Models/Sale.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Sale extends Model
+{
+    protected $fillable = [
+        'quantity',
+        'product_id'
+    ];
+    use HasFactory;
+}
